f4ee4707145543e08c74992d6bd2221518e06b6b
ViBH2204 <ahluwaliavaibhav22@gmail.com>
2025-11-21 02:34:16 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,4 +2,6 @@
 # Put your private keys here (do NOT commit to public repos).
 
 # OPENAI_API_KEY = "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"
+
 NEWS_API_KEY = "1c7fdb04043c405f9969b8856c118cee"  
+WEATHER_API_KEY = "64b03b90230b0e75a25871bcbad7fa8d"
